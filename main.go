5073dc10da277c60fe60abcf62fff68019803c6d
Morpheus Zein <morpheus.zein@irawan.id>
2024-01-31 23:26:49 +0700

    Testing out classes and pointers in go lmaooo

--- main.go
+++ main.go
@@ -11,25 +11,32 @@ type Film struct {
   Title string
   Director string
 }
+type Films struct {
+  Filems []Film
+}
+
+var Filmx Films
+
+func (f *Films) addfilm(tit,dir string) *Films {
+  f.Filems = append(f.Filems, []Film{
+    {Title: tit,Director: dir},
+  }...)
+  return f
+}
 
 func main(){
   var h1 http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
     tmpl := template.Must(template.ParseFiles("index.html")) 
-    films := map[string][]Film{
-      "Films": {
-        {Title: "Black Panther", Director: "Ryan Coogler"},
-        {Title: "Fight Club", Director: "David Fincher"},
-        {Title: "13 Bom Di Jakarta", Director: "Angga Dimas Sasongko"},
-      },
-    }
-
-    if err := tmpl.Execute(w,films); err != nil {
-      http.Error(w,err.Error(),http.StatusInternalServerError)
+    if err:= tmpl.Execute(w,Filmx); err != nil {
+      panic(err)
     }
   }
+
   var h2 http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
     title := r.PostFormValue("title")
     director := r.PostFormValue("director")
+    Filmx.addfilm(title,director)
+
     htmlStrx := fmt.Sprintf("<p class='px-3 py-2'>%s - %s</p>", title,director)
     tmepx,_ := template.New("res").Parse(htmlStrx)
     tmepx.Execute(w,nil)
