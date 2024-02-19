d5f6bb2b43036e4fc6ca770319362dbf1f8975b0
Morpheus Zein <morpheus.zein@irawan.id>
2024-02-19 15:08:08 +0700

    finally done doing ts man i loved it so much

--- main.go
+++ main.go
@@ -18,19 +18,19 @@ type TodoList struct {
 	TodoList []Todo
 }
 
-var Filmx TodoList = TodoList{
+var Todoz TodoList = TodoList{
 	TodoList: []Todo{
 		{TodoID: uuid.New(), Title: "Wash Da Dishes", Todo: "Wash Da fucking Dishes"},
 	},
 }
 
-func (f *TodoList) addfilm(tit, dir string) uuid.UUID {
-	newFilm := Todo{TodoID: uuid.New(), Title: tit, Todo: dir}
-	f.TodoList = append(f.TodoList, newFilm)
-	return newFilm.TodoID
+func (f *TodoList) addTodo(tit, dir string) uuid.UUID {
+	newTodo := Todo{TodoID: uuid.New(), Title: tit, Todo: dir}
+	f.TodoList = append(f.TodoList, newTodo)
+	return newTodo.TodoID
 }
 
-func (f *TodoList) DeleteFilm(FilmID uuid.UUID) {
+func (f *TodoList) DeleteTodo(FilmID uuid.UUID) {
 	for i, v := range f.TodoList {
 		if v.TodoID == FilmID {
 			f.TodoList = append(f.TodoList[:i], f.TodoList[i+1:]...)
@@ -38,49 +38,75 @@ func (f *TodoList) DeleteFilm(FilmID uuid.UUID) {
 	}
 }
 
+func (f *TodoList) EditTodo(eTodoID uuid.UUID, tit, tod string) {
+	EditedTodo := Todo{TodoID: eTodoID, Title: tit, Todo: tod}
+	for a, v := range f.TodoList {
+		if v.TodoID == eTodoID {
+			f.TodoList[a] = EditedTodo
+		}
+	}
+}
+
 func main() {
 	var h1 http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		if err := tmpl.Execute(w, Filmx); err != nil {
+		if err := tmpl.Execute(w, Todoz); err != nil {
 			panic(err)
 		}
 	}
 
 	var h2 http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
 		title := r.PostFormValue("title")
-		director := r.PostFormValue("director")
+		todo := r.PostFormValue("todo")
 
-		Flz := Filmx.addfilm(title, director)
+		Flz := Todoz.addTodo(title, todo)
 		childe, _ := template.ParseFiles("comps/child.html")
 		res := TodoList{
 			TodoList: []Todo{
-				{Title: title, Todo: director, TodoID: Flz},
+				{Title: title, Todo: todo, TodoID: Flz},
 			},
 		}
 
 		if err := childe.ExecuteTemplate(w, "A", res); err != nil {
 			log.Panic(err)
 		} else {
-			log.Printf("Todo item created. Values : \n{ Title: %v, Todo: %v, TodoID: %v }\n\n", title, director, Flz)
+			log.Printf("Todo item created.Values : \n{ Title: %v, Todo: %v, TodoID: %v }\n\n", title, todo, Flz)
 		}
 	}
 
 	var h3 http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
 		aidi := r.URL.Query().Get("id")
 		idd, _ := uuid.Parse(aidi)
-		Filmx.DeleteFilm(idd)
+		Todoz.DeleteTodo(idd)
 
-		t, _ := template.New("foo").ParseFiles("comps/child.html")
-		if err := t.ExecuteTemplate(w, "B", Filmx); err != nil {
+		t, _ := template.New("grahh").ParseFiles("comps/child.html")
+		if err := t.ExecuteTemplate(w, "B", Todoz); err != nil {
 			log.Panic(err)
 		} else {
 			log.Printf("Todo item '%v' was Deleted. \n\n", aidi)
 		}
 	}
 
+	var h4 http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		eid := r.URL.Query().Get("eid")
+		idd, _ := uuid.Parse(eid)
+		tit := r.URL.Query().Get("title")
+		tod := r.URL.Query().Get("todo")
+
+		Todoz.EditTodo(idd, tit, tod)
+
+		t, _ := template.New("grahh").ParseFiles("comps/child.html")
+		if err := t.ExecuteTemplate(w, "B", Todoz); err != nil {
+			log.Panic(err)
+		} else {
+			log.Printf("Todo item '%v' was Edited. \n\n", eid)
+		}
+	}
+
 	http.HandleFunc("/", h1)
 	http.HandleFunc("/add-film", h2)
 	http.HandleFunc("/del", h3)
+	http.HandleFunc("/edt", h4)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
